7f44b161dcd732a4ea51eec8151e3d99eb13e622
Jordan Needham <jdncreations@hotmail.com>
2022-06-12 10:55:07 +0800

    basic UI structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,13 @@
 export default function Home() {
-  return <div className=''>Hello world</div>;
+  return (
+    <div className='h-screen w-screen flex flex-col justify-center max-w-2xl items-center'>
+      <div className=' text-2xl text-center'>Which Pokemon is Rounder?</div>
+      <div className='p-2'></div>
+      <div className='border rounded p-8 flex justify-between'>
+        <div className='w-16 h-16 bg-red-200'></div>
+        <div className='p-8'>Vs</div>
+        <div className='w-16 h-16 bg-red-200'></div>
+      </div>
+    </div>
+  );
 }
